fc125eaaae7538f441b214343100898346e794ad
Robert Stachurski <you@example.com>
2022-04-10 15:36:42 -0400

    Finisehd preprocessing, started the CNN model

--- model/model.py
+++ model/model.py
@@ -1,12 +1,70 @@
+import pandas as pd
 import numpy as np
-import keras
-from keras import optimizers
-from keras.datasets import mnist
-from keras.layers.core import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPool2D
-from keras.models import Sequential
-from tensorflow.keras.utils import to_categorical
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import seaborn as sns
+%matplotlib inline
 
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
-print(X_train)
-print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
+np.random.seed(2)
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import itertools
+
+from keras.utils.np_utils import to_categorical # convert to one-hot-encoding
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
+# from keras.optimizers import RMSprop
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ReduceLROnPlateau
+
+
+train = pd.read_csv('train.csv')
+test = pd.read_csv("test.csv")
+Y_train = train["label"]
+X_train = train.drop(labels = ["label"],axis = 1) 
+
+print(Y_train.value_counts())
+
+Y_train.value_counts().plot(kind='bar')
+
+print(train.isnull().sum())
+print(train.info())
+
+X_train = X_train / 255.0
+test = test / 255.0
+
+X_train = X_train.values.reshape(-1,28,28,1)
+test = test.values.reshape(-1,28,28,1)
+
+Y_train = to_categorical(Y_train, num_classes = 10)
+random_seed = 2
+
+X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size = 0.1, random_state=random_seed)
+g = plt.imshow(X_train[0][:,:,0])
+
+model = Sequential()
+
+model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same', 
+                 activation ='relu', input_shape = (28,28,1)))
+model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same', 
+                 activation ='relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+
+model.add(Conv2D(filters = 64, kernel_size = (3,3),padding = 'Same', 
+                 activation ='relu'))
+model.add(Conv2D(filters = 64, kernel_size = (3,3),padding = 'Same', 
+                 activation ='relu'))
+model.add(MaxPool2D(pool_size=(2,2), strides=(2,2)))
+model.add(Dropout(0.25))
+
+
+model.add(Flatten())
+model.add(Dense(256, activation = "relu"))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation = "softmax"))
+# (X_train, y_train), (X_test, y_test) = mnist.load_data()
+# print(X_train)
+# print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
